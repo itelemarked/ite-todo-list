02023d503fcf2c8ba07d29354ec4089187bfdc93
itelemarked <itelemarked@icloud.com>
2024-04-21 23:12:03 +0200

    firebase installed

---
+++ src/modules/Libraries/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAP7cEDguAxZlFQAh88r7R3ApNC3RXdW-Y',
+  authDomain: 'ite-todo-f9e2c.firebaseapp.com',
+  databaseURL: 'https://ite-todo-f9e2c-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'ite-todo-f9e2c',
+  storageBucket: 'ite-todo-f9e2c.appspot.com',
+  messagingSenderId: '172881690257',
+  appId: '1:172881690257:web:c01998927a788abcf58930'
+}
+
+const app = initializeApp(firebaseConfig)
+
+export const db = getFirestore(app)
